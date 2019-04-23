11941c456dda50a5e12fd65591d0c2b495649d9e
Damien Chambon <damienchambon@d-172-25-202-244.dhcp.virginia.edu>
2019-04-23 12:18:01 -0400

    Updated Python file

--- src/project.py
+++ src/project.py
@@ -13,6 +13,11 @@ import glob
 import numpy as np
 from time import sleep #prevents exceeding data rate limit
 from IPython.display import clear_output
+import matplotlib.pyplot as plt
+
+##################
+## DATA LOADING ##
+##################
 
 def read_stimuli(path):
     # This function reads the different stimuli that a neuron has been tested with
@@ -47,30 +52,31 @@ def read_area(path):
 # path: folder where the different areas are located
     arrayNeur = []
     for neuronName in os.listdir(path):
-        # for each neuron in the folder
-        if os.name == 'nt':
-            # for Windows
-            arrayFolder = glob.glob(path+"\\"+neuronName+"\\"+"*")
-        else:
-            arrayFolder = glob.glob(path+"/"+neuronName+"/"+"*")
-        listTypes = []
-        for folder in arrayFolder:
-            # get the list of the types of stimuli that the neuron was tested with
+        if neuronName != ".DS_Store":
+            # for each neuron in the folder
             if os.name == 'nt':
                 # for Windows
-                listTypes.append(folder[len(path+"\\"+neuronName+"\\"):])
+                arrayFolder = glob.glob(path+"\\"+neuronName+"\\"+"*")
             else:
-                listTypes.append(folder[len(path+"/"+neuronName+"/"):])
-        arrayStimuli = []
-        for t in listTypes:
-            # for each type in the list, read the spikes generated and the stimuli tested
-            if os.name == 'nt':
-                # for Windows
-                arraySpikes = read_stimuli(path+"\\"+neuronName+"\\"+t)
-            else:
-                arraySpikes = read_stimuli(path+"/"+neuronName+"/"+t)
-            arrayStimuli.append({"type": t, "spikes": arraySpikes})
-        arrayNeur.append({"name": neuronName, "stimuli": arrayStimuli})
+                arrayFolder = glob.glob(path+"/"+neuronName+"/"+"*")
+                listTypes = []
+                for folder in arrayFolder:
+                    # get the list of the types of stimuli that the neuron was tested with
+                    if os.name == 'nt':
+                        # for Windows
+                        listTypes.append(folder[len(path+"\\"+neuronName+"\\"):])
+                    else:
+                        listTypes.append(folder[len(path+"/"+neuronName+"/"):])
+                arrayStimuli = []
+                for t in listTypes:
+                    # for each type in the list, read the spikes generated and the stimuli tested
+                    if os.name == 'nt':
+                        # for Windows
+                        arraySpikes = read_stimuli(path+"\\"+neuronName+"\\"+t)
+                    else:
+                        arraySpikes = read_stimuli(path+"/"+neuronName+"/"+t)
+                    arrayStimuli.append({"type": t, "spikes": arraySpikes})
+                    arrayNeur.append({"name": neuronName, "stimuli": arrayStimuli})
     return arrayNeur
 
 listAreas = ["Field_L_cells","MLd_cells"] # list of areas to get the data from
@@ -94,3 +100,53 @@ for area in listAreas:
 sleep(1.5) # keep output for some time    
 clear_output()
 print("Loading of the data done!")
+
+#################
+## RASTER PLOT ##
+#################
+
+import graphics
+
+nb_flatrip = 0
+nb_conspecific = 0
+
+nb_flatrip = len(arrayAreas[0]["neurons"][0]["stimuli"][0]["spikes"])
+nb_conspecific = len(arrayAreas[0]["neurons"][0]["stimuli"][1]["spikes"])
+print(nb_flatrip)
+print(nb_conspecific)
+n_rows = min(nb_conspecific,nb_flatrip)
+
+fig, axes = plt.subplots(nrows=n_rows, ncols=2, sharex=True, figsize=(15, 9))
+
+# code inspired by https://stackoverflow.com/questions/31006971/setting-the-same-axis-limits-for-all-subplots-in-matplotlib/31007233
+for (m,n), subplot in np.ndenumerate(axes):
+    subplot.set_xlim(-2000,4000) # sets the xlim between -2000 and 4000
+
+for (m,n), subplot in np.ndenumerate(axes):
+    if m == 0:
+        if n == 0:
+            subplot.set_title("Flatrip (synthetic)",fontsize=20) # sets the title of left column
+        else:
+            subplot.set_title("Conspecific (authentic)",fontsize=20) # sets the title of left column
+    if m == (n_rows-1):
+        subplot.set_xlabel("Milliseconds",fontsize=15) # sets the title of left column
+
+listNeurons_field_l_neuron1 = arrayAreas[0]["neurons"][1]["stimuli"]
+#print(listNeurons_field_l[0]["stimuli"])
+
+index_stimuli = 0
+while index_stimuli < n_rows:
+    # flatrip is white noise (synthetic)
+    spikes_flatrip = listNeurons_field_l_neuron1[0]["spikes"][index_stimuli]["seriesSpikes"]
+    graphics.plot_raster(axes[index_stimuli][0],spikes_flatrip)
+    # conspecific is authentic sound
+    spikes_conspecific = listNeurons_field_l_neuron1[1]["spikes"][index_stimuli]["seriesSpikes"]
+    graphics.plot_raster(axes[index_stimuli][1],spikes_conspecific)
+    index_stimuli+=1
+    
+    
+#####################
+## DISTANCE MATRIX ##
+#####################
+import pyspike
+
